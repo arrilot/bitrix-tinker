4abe327fc21967ce1a8463736cea0c066f9941aa
Template Server <template@greensight.ru>
2019-03-28 18:57:48 +0300

    0.1

--- src/Console/TinkerCommand.php
+++ src/Console/TinkerCommand.php
@@ -2,7 +2,11 @@
 
 namespace Arrilot\BitrixTinker\Console;
 
+use Arrilot\BitrixTinker\ClassAliasAutoloader;
+use Psy\Configuration;
+use Psy\Shell;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -14,7 +18,12 @@ class TinkerCommand extends Command
     protected function configure()
     {
         $this->setName('tinker')
-            ->setDescription('Interact with your application');
+            ->setDescription('Interact with your application')
+            ->addArgument(
+                'include',
+                InputArgument::IS_ARRAY,
+                'Include file(s) before starting tinker'
+            );;
     }
 
     /**
@@ -27,6 +36,48 @@ class TinkerCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // TODO
+        $this->getApplication()->setCatchExceptions(false);
+        $config = new Configuration([
+            'updateCheck' => 'never',
+            'errorLoggingLevel' => E_ALL & ~E_NOTICE & ~E_WARNING & ~E_STRICT & ~E_USER_WARNING & ~E_USER_NOTICE & ~E_COMPILE_WARNING & ~E_DEPRECATED,
+        ]);
+        $config->getPresenter()->addCasters($this->getCasters());
+        $shell = new Shell($config);
+        $includes = $input->getArgument('include');
+        $shell->setIncludes($includes);
+
+        $path = $this->vendorPath().DIRECTORY_SEPARATOR.'composer/autoload_classmap.php';
+        $loader = ClassAliasAutoloader::register($shell, $path);
+        try {
+            $shell->run();
+        } finally {
+            $loader->unregister();
+        }
+    }
+
+    /**
+     * @return bool|string
+     */
+    protected function vendorPath()
+    {
+        return dirname(dirname(dirname(dirname(__DIR__))));
+    }
+
+    /**
+     * Get an array of Laravel tailored casters.
+     *
+     * @return array
+     */
+    protected function getCasters()
+    {
+        $casters = [];
+        if (class_exists('Illuminate\Support\Collection')) {
+            $casters['Illuminate\Support\Collection'] = 'Laravel\Tinker\TinkerCaster::castCollection';
+        }
+        if (class_exists('Illuminate\Database\Eloquent\Model')) {
+            $casters['Illuminate\Database\Eloquent\Model'] = 'Laravel\Tinker\TinkerCaster::castModel';
+        }
+
+        return $casters;
     }
 }
